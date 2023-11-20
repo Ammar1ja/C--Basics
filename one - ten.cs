729fc6964b0a93077df7b07954078021c3ef20c0
Ammar Jaber <jaberr.ammar22@gmail.com>
2023-11-20 18:19:36 +0300

    1-10

---
+++ one - ten.cs
@@ -0,0 +1,40 @@
+﻿internal class Program
+
+{
+    private static void Main(string[] args)
+
+    {
+        /*
+        - High Level Languages
+            Machine Language[0,1] <=> [compiler] <=> high level languages[c#, c++, java...]
+        - Curly Braces
+            {}
+        - Integrated Development Environment (IDE)
+            Examples:
+            1.Visual Studio 
+            2.Visual Studio Code
+            3.Atom
+        - Graphical User Interface (GUI)
+        - Console:
+            Examples
+            1. cmd
+            2. terminal
+        - Main: is the main method that will be called once you run the project.
+        */
+
+        // - Comments:
+        // 1. inline Comment: one line comment => //
+        // 2. Multi Line Comment: multi line comment => /**/
+
+
+
+
+        // Print text in console using [x] language
+        // This is print statement
+        // WriteLine: print and go to the next line
+        Console.WriteLine("Hello ");
+        // Write: print and stay at the same line
+        Console.Write("Ammar");
+        Console.WriteLine(" Jaber");
+    }
+}
